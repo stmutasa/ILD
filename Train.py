849c2533e4abc37c0220d4f037baf5cc1579ea57
Simi Mutasa <stmutasa@gmail.com>
2019-10-04 13:51:28 -0400

    Fin.

--- Train.py
+++ Train.py
@@ -28,8 +28,8 @@ tf.app.flags.DEFINE_integer('network_dims', 40, """dimensions of the network inp
 tf.app.flags.DEFINE_integer('num_classes', 2, """Number of classes""")
 
 # Define some of the immutable variables
-tf.app.flags.DEFINE_integer('num_epochs', 410, """Number of epochs to run""")
-tf.app.flags.DEFINE_integer('epoch_size', 93000, """How many examples""")
+tf.app.flags.DEFINE_integer('num_epochs', 300, """Number of epochs to run""")
+tf.app.flags.DEFINE_integer('epoch_size', 114000, """How many examples""")
 tf.app.flags.DEFINE_integer('print_interval', 5, """How often to print a summary to console during training""")
 tf.app.flags.DEFINE_integer('checkpoint_interval', 15, """How many Epochs to wait before saving a checkpoint""")
 tf.app.flags.DEFINE_integer('batch_size', 512, """Number of images to process in a batch.""")
@@ -48,7 +48,7 @@ tf.app.flags.DEFINE_float('beta2', 0.999, """ The beta 1 value for the adam opti
 
 # Directory control
 tf.app.flags.DEFINE_string('train_dir', 'training/', """Directory to write event logs and save checkpoint files""")
-tf.app.flags.DEFINE_string('RunInfo', 'Wedge_1_6/', """Unique file name for this training run""")
+tf.app.flags.DEFINE_string('RunInfo', 'Final_Network/', """Unique file name for this training run""")
 tf.app.flags.DEFINE_integer('GPU', 0, """Which GPU to use""")
 
 def train():
@@ -102,7 +102,7 @@ def train():
         var_restore = var_ema.variables_to_restore()
 
         # Initialize the saver
-        saver = tf.train.Saver(var_restore, max_to_keep=8)
+        saver = tf.train.Saver(var_restore, max_to_keep=10)
 
         # -------------------  Session Initializer  ----------------------
 
